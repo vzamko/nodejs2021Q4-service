5ed06bdb997e6970dd643050cd1410e3c2c72c33
Valera Zamko <valera.zamko@jet.dev>
2021-12-03 13:17:25 +0200

    feat: Base implementation of users

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,22 +1,25 @@
-const uuid = require('uuid');
+const {v4: uuidv4} = require('uuid');
 
 class User {
-  constructor({
-    id = uuid(),
-    name = 'USER',
-    login = 'user',
-    password = 'P@55w0rd'
-  } = {}) {
+  constructor({id = uuidv4(), name = 'USER', login = 'user', password = 'P@55w0rd'} = {}) {
     this.id = id;
     this.name = name;
     this.login = login;
     this.password = password;
-  }
+  };
 
-  static toResponse(user) {
-    const { id, name, login } = user;
+  setProperty(property) {
+    this.name = property.name ?? this.name;
+    this.login = property.login ?? this.login;
+    this.password = property.password ?? this.password;
+  };
+
+  toResponse() {
+    const { id, name, login } = this;
+
     return { id, name, login };
-  }
+  };
+
 }
 
 module.exports = User;
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,44 @@
-const router = require('express').Router();
-const User = require('./user.model');
+const Router = require('@koa/router');
 const usersService = require('./user.service');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+const router = new Router({
+  prefix: '/users'
+});
+
+router.get('/', ctx => {
+  const users = usersService.getAll();
+
+  ctx.body = users.map(user => (user.toResponse()));
+});
+
+router.get('/:id', ctx => {
+  const {id} = ctx.params;
+
+  ctx.body = usersService.getUserById(id).toResponse();
+});
+
+router.post('/', ctx => {
+  let user = ctx.request.body;
+  user = usersService.createUser(user);
+  ctx.response.status = 201;
+  ctx.body = user.toResponse();
+});
+
+router.put('/:id', ctx => {
+  const {id} = ctx.params;
+
+  const user = usersService.updateUser(id, ctx.request.body);
+
+  ctx.response.status = 200;
+  ctx.body = user.toResponse();
+});
+
+router.delete('/:id', ctx => {
+  const {id} = ctx.params;
+
+  usersService.deleteUser(id);
+
+  ctx.response.status = 204;
 });
 
 module.exports = router;
